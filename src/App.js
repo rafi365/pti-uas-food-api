4e6c65b583cc5620799f57b51b511acf177aa45b
rafi365 <muhammad.rafi3@student.umn.ac.id>
2020-12-07 23:11:54 +0700

    added nav bar

--- src/App.js
+++ src/App.js
@@ -5,47 +5,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <nav className="navbar">
+          <ol>
+            <ul className="nav"><a href="#">Home</a></ul>
+            <ul className="nav"><a href="#">Food</a></ul>
+            <ul className="nav"><a href="#">About</a></ul>
+          </ol>
+        </nav>
+      </div>
       <header className="App-header">
         <h1>FOOD DATABASE</h1>
-        {/* <div>
-          <form action="https://youtube.com/results">
-            <label for="cari">Search Videos on Youtube : </label>
-            <input name="search_query" target="_blank"></input>
-            <button>Search Youtube</button>
-          </form>
-        </div>
-        <div>
-          <form action="https://lmgtfy.app/">
-            <label for="cari">Search google : </label>
-            <input name="q"></input>
-            <button>Search google</button>
-          </form>
-        </div> */}
-        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/NUYvbT6vTPs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-        <p>So... Nothing's happening {1+1}</p>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
+        <p>Healthy Food, Healthy Life.</p>
+
+        
       </header>
-<div class="container">
-    <nav class="navbar">
-        <ol>
-            <ul>Home</ul>
-            <ul>About</ul>
-            <ul>what else...</ul>
-        </ol>
-    </nav>
-</div>
-    </div>
+      
+      <div className="main"></div>
+        
+      </div>
+
   );
 }
 
