8a2009b6a9b2b538bb501d45c3e8110e64cead74
rafi365 <74255365+rafi365@users.noreply.github.com>
2020-12-16 14:04:17 +0700

    functional search

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React,{Component, Fragment} from 'react';
 import Header from './Header';
 import Recipes from './Recipes';
 import axios from 'axios';
+import './App.css'
 const API_KEY = '9b904d703fa0d46a88ce1ac63f29f498';
     const API_ID = '4e9f05eb';
-    const query = 'tomato'
-    const url = `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`;
 
 class App extends React.Component {
   constructor(props) {
@@ -13,11 +12,24 @@ class App extends React.Component {
 
     this.state = {
       data: null,
-      externalData: null,
+      searchinput:'',
+      SearchEmpty: true,
     };
   }
-  componentDidMount(){
-    axios.get(url)
+
+  handleSearch(event) {
+    const inputValue = event.target.value;
+    const isEmpty = inputValue === '';
+    this.setState({
+      searchinput: inputValue,
+      SearchEmpty: isEmpty,
+    });
+  }
+
+  getsearch(){
+    let query;
+    query = this.state.searchinput;
+    axios.get(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`)
         .then(result => {
             this.setState({ 
                 data: result.data
@@ -27,12 +39,6 @@ class App extends React.Component {
         .catch(console.error)
   }
   render() {
-    if (this.state.data === null) {
-      // Render loading state ...
-      return(
-        <p>loading....</p>
-      );
-    } else {
       // Render real UI ...
       return (
         <div className="App">
@@ -41,21 +47,30 @@ class App extends React.Component {
           <h1>FOOD DATABASE</h1>
   
           <p>Healthy Food, Healthy Life.</p>
+          <input
+            value={this.state.searchinput}
+            onChange={(event) => {this.handleSearch(event)}}
+          />
+          <input
+          type='button'
+          value='Search'
+          onClick={()=>{this.getsearch()}}
+        />
         </header>
         
         <div className="main">
         </div>
-        
-        {this.state.data.hits.map(data => {
-                  return(
-                    <Recipes 
-                    recipe={data.recipe}
-                    />
-                  );
-              })}
+        { this.state && this.state.data &&
+              <div>{this.state.data && this.state.data.hits.map(data => {
+                return(
+                  <Recipes 
+                  recipe={data.recipe}
+                  />
+                );
+            })}</div>
+        }
         </div>
       );
-    }
   }
 }
 
